8caf4d00720001846f57763abac94f6c909065d5
point2-pixel <cs2point@gmail.com>
2026-07-17 09:45:37 +0500

    Create page.tsx

---
+++ app/dashboard/competitor-watch/page.tsx
@@ -0,0 +1,9 @@
+import { CompetitorWatch } from "@/components/dashboard/CompetitorWatch";
+
+export default function CompetitorWatchPage() {
+  return (
+    <div className="p-8">
+      <CompetitorWatch />
+    </div>
+  );
+}
